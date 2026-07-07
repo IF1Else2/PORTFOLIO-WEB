32a2c100ae5650c09d57185e1a8307b3065837a3
IreneJC <titealuche@gmail.com>
2026-07-08 00:12:56 +0200

    update src folder

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -25,11 +25,28 @@ export class Projects {
     'assets/images/ch2.png'
   ];
 
+   /** Carrusel del Proyecto 4 */
+  projectImages3: string[] = [
+    'assets/images/pbiv1.png',
+    'assets/images/pbiv2.png'
+  ];
+
+/** Carrusel del Proyecto 5 */
+  projectImages4: string[] = [
+    'assets/images/m_5.png',
+    'assets/images/n_1.png',
+    'assets/images/n2.png',
+    'assets/images/k_5.png',
+    'assets/images/g_1.png',
+    'assets/images/d_b_5.png'
+  ];
+
   /** Índices actuales de cada carrusel */
   currentIndex: number = 0;
   currentIndex1: number = 0;
   currentIndex2: number = 0;
-
+  currentIndex3: number = 0;
+  currentIndex4: number = 0;
   /** Navega hacia la imagen anterior del Proyecto 1 */
   prevImage() {
     this.currentIndex =
@@ -65,4 +82,29 @@ export class Projects {
     this.currentIndex2 =
       (this.currentIndex2 + 1) % this.projectImages2.length;
   }
+
+  /** Navega hacia la imagen anterior del Proyecto 4 */
+  prevImage3() {
+    this.currentIndex3 =
+      (this.currentIndex3 - 1 + this.projectImages3.length) % this.projectImages3.length;
+  }
+
+  /** Navega hacia la imagen siguiente del Proyecto 4 */
+  nextImage3() {
+    this.currentIndex3 =
+      (this.currentIndex3 + 1) % this.projectImages3.length;
+  }
+
+   /** Navega hacia la imagen anterior del Proyecto 5 */
+  prevImage4() {
+    this.currentIndex4 =
+      (this.currentIndex4 - 1 + this.projectImages4.length) % this.projectImages4.length;
+  }
+
+  /** Navega hacia la imagen siguiente del Proyecto 5 */
+  nextImage4() {
+    this.currentIndex4 =
+      (this.currentIndex4 + 1) % this.projectImages4.length;
+  }
+
 } 
